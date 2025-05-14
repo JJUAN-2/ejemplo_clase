ee96f4892720d18c97c763adf3169b95aa2d165c
JJUAN-2 <juan.diaz_g@uao.edu.co>
2025-05-14 16:39:25 -0500

    Agregamos tkinter a la visualizacion

--- visualizacion.py
+++ visualizacion.py
@@ -5,12 +5,33 @@ import matplotlib.pyplot as plt
 # seaborn para visualizacion mas estilizadas 
 import seaborn as sns
 import os
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
+
+
 
 from Analisis import DataAnalyzer
 
 data = pd.read_csv("adult.csv")
 analizar = DataAnalyzer(data)
 
-analizar.summary()
-analizar.correlation_matrix()
-analizar.categorical_analisis()
+info = analizar.summary()
+
+def informacion():
+        
+        text_area.delete("1.0", tk.END) #Para vaciar al ejecutar 
+        info = analizar.summary()
+        text_area.insert(tk.END, info)
+  
+
+
+ventana = tk.Tk()
+ventana.title("Analizis de datos")
+boton_summary = tk.Button(ventana, text= "info", command= informacion)
+boton_summary.pack()
+
+text_area = tk.scrolledtext.ScrolledText(ventana, width = 70, height = 30, )
+
+text_area.pack()
+ventana.mainloop()
